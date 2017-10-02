a527097fd290f326fd8298d0ed83adf485779b85
feel5ny <feel5.nayoung@gmail.com>
2017-10-03 01:30:33 +0900

    로그인 페이지 완료

--- frontend/src/pages/LoginPage/index.js
+++ frontend/src/pages/LoginPage/index.js
@@ -2,56 +2,96 @@ import React from 'react'
 import {
   Button,
   Header,
-  Form,
   Grid,
 } from 'semantic-ui-react'
 import * as Fa from 'react-icons/lib/fa'
+import './Login.css'
+import bgVideo from '../../static/video/pencil_down.mp4'
+import bgImg from '../../static/img/login_img.jpg'
+
 const LoginPage = () => (
-  <div className="login-form">
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
+  <div className="login-wrapper">
+
+  </div>
+  <div className="login">
+    <video autoPlay loop poster={bgImg}>
+      <source src={bgVideo} type="video/mp4" />
+    </video>
     <Grid
-      textAlign="center"
-      style={{ height: '100%' }}
-      verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header
-          as="h2"
-          color="#1a273c"
-          textAlign="center">
-          Da, Da
-        </Header>
-        <Form size="large">
-          <Button
-            color="facebook"
-            fluid
-            size="large">
-            <Fa.FaFacebookSquare />
-            Facebook 계정으로 로그인 하기
-          </Button>
-          <Button
-            color="instagram"
-            fluid
-            size="large">
-            <Fa.FaInstagram />
-            Instagram 계정으로 로그인 하기
-          </Button>
-          <Button positive fluid size="large">
-            Naver 계정으로 로그인 하기
-          </Button>
-          <Button
-            color="yellow"
-            fluid
-            size="large">
-            Kakao 계정으로 로그인 하기
-          </Button>
-        </Form>
-      </Grid.Column>
+      divided="vertically"
+      textAlign="left"
+      style={{ marginTop: '127px' }}
+    >
+      <Grid.Row columns={4}>
+        <Grid.Column>
+          <Header
+            as="h1"
+            className="login-header"
+            style={{
+              fontSize: '60px',
+              fontFamily: 'montserrat',
+              fontWeight: '600',
+              color: '#fff',
+            }}
+          >
+            <Header.Subheader
+              style={{
+                color: '#fff',
+                fontSize: '21px',
+              }}
+            >
+              건강하고 올바른 <br />
+              다이어트를 위한 식단 <br />
+              다이어리 서비스<br />
+            </Header.Subheader>
+            DA,DA
+          </Header>
+          <div className="login-socialBtns">
+            <Button
+              fluid
+              style={{
+                backgroundImage:
+                  'linear-gradient(261deg, #0f2e6c, #4267b2)',
+                color: '#fff',
+              }}
+            >
+              {/* <Fa.FaFacebookSquare /> */}
+              Facebook 계정으로 로그인 하기
+            </Button>
+            <Button
+              fluid
+              style={{
+                backgroundImage:
+                  'linear-gradient(279deg, #515bd4, #8134af 23%, #dd2a7b 48%, #feda77)',
+                color: '#fff',
+              }}
+            >
+              {/* <Fa.FaInstagram /> */}
+              Instagram 계정으로 로그인 하기
+            </Button>
+            <Button
+              fluid
+              style={{
+                backgroundImage:
+                  'linear-gradient(261deg, #008215, #00c73c)',
+                color: '#fff',
+              }}
+            >
+              Naver 계정으로 로그인 하기
+            </Button>
+            <Button
+              fluid
+              style={{
+                backgroundImage:
+                  'linear-gradient(261deg, #ffb600, #fae200)',
+                color: '#16325c',
+              }}
+            >
+              Kakao 계정으로 로그인 하기
+            </Button>
+          </div>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   </div>
 )
