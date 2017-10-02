613b8e440e0362c370bd0e4650d71656099aa3fc
feel5ny <feel5.nayoung@gmail.com>
2017-10-03 02:04:08 +0900

    로그인페이지 퍼블리싱 완료

--- frontend/src/pages/LoginPage/index.js
+++ frontend/src/pages/LoginPage/index.js
@@ -6,13 +6,10 @@ import {
 } from 'semantic-ui-react'
 import * as Fa from 'react-icons/lib/fa'
 import './Login.css'
-import bgVideo from '../../static/video/pencil_down.mp4'
+import bgVideo from '../../static/video/bg_login.mp4'
 import bgImg from '../../static/img/login_img.jpg'
 
 const LoginPage = () => (
-  <div className="login-wrapper">
-
-  </div>
   <div className="login">
     <video autoPlay loop poster={bgImg}>
       <source src={bgVideo} type="video/mp4" />
@@ -38,11 +35,18 @@ const LoginPage = () => (
               style={{
                 color: '#fff',
                 fontSize: '21px',
+                fontWeight: '100',
+                marginBottom: '10px',
+                lineHeight: '1.35',
               }}
             >
               건강하고 올바른 <br />
-              다이어트를 위한 식단 <br />
-              다이어리 서비스<br />
+              <span style={{ fontWeight: '700' }}>
+                다
+              </span>이어트를 위한 식단 <br />
+              <span style={{ fontWeight: '700' }}>
+                다
+              </span>이어리 서비스<br />
             </Header.Subheader>
             DA,DA
           </Header>
@@ -53,6 +57,7 @@ const LoginPage = () => (
                 backgroundImage:
                   'linear-gradient(261deg, #0f2e6c, #4267b2)',
                 color: '#fff',
+                fontWeight: '100',
               }}
             >
               {/* <Fa.FaFacebookSquare /> */}
@@ -64,6 +69,7 @@ const LoginPage = () => (
                 backgroundImage:
                   'linear-gradient(279deg, #515bd4, #8134af 23%, #dd2a7b 48%, #feda77)',
                 color: '#fff',
+                fontWeight: '100',
               }}
             >
               {/* <Fa.FaInstagram /> */}
@@ -75,6 +81,7 @@ const LoginPage = () => (
                 backgroundImage:
                   'linear-gradient(261deg, #008215, #00c73c)',
                 color: '#fff',
+                fontWeight: '100',
               }}
             >
               Naver 계정으로 로그인 하기
@@ -85,6 +92,7 @@ const LoginPage = () => (
                 backgroundImage:
                   'linear-gradient(261deg, #ffb600, #fae200)',
                 color: '#16325c',
+                fontWeight: '100',
               }}
             >
               Kakao 계정으로 로그인 하기
