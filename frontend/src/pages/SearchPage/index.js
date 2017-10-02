0e6210b1bf07f81d5c154886533f263367b5d802
huusz <irinso93@gmail.com>
2017-10-03 03:25:24 +0900

    search화면 배경 추가 및 구성요소 추가 레이아웃 미완

--- frontend/src/pages/SearchPage/index.js
+++ frontend/src/pages/SearchPage/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import {
+  Search,
+  Grid,
+  Header,
+} from 'semantic-ui-react'
 import Navigation from '../../components/Navigation'
 import bgImg from '../../static/img/search_bg.jpg'
 import './Search.css'
@@ -21,7 +26,49 @@ class SearchPage extends Component {
           inverted="true"
           color="#fff"
         />
-        <div>SearchPage</div>
+
+        <Grid class="search-grid-center">
+          <Grid.Column width={4} />
+          <Grid.Column
+            width={8}
+            className="search-grid-center">
+            <Header
+              style={{
+                fontFamily: 'Spoqa Han Sans',
+                fontWeight: '100',
+                fontSize: '56px',
+                margin: '0',
+              }}
+              as="h1"
+              content="어떤 요리를 원하세요?"
+              inverted
+            />
+            <Header
+              style={{
+                fontFamily: 'Spoqa Han Sans',
+                fontWeight: '100',
+                fontSize: '28px',
+                margin: '0',
+              }}
+              as="h5"
+              content="레시피를 검색해보세요."
+              inverted
+            />
+            <Search />
+            <Header
+              style={{
+                fontFamily: 'Spoqa Han Sans',
+                fontWeight: '100',
+                fontSize: '14px',
+                margin: '0',
+              }}
+              as="h6"
+              content="하루 권장량 기준, 현재 소비가능한 칼로리는 300kcal입니다."
+              inverted
+            />
+          </Grid.Column>
+          <Grid.Column width={4} />
+        </Grid>
       </div>
     )
   }
