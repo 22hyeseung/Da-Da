1b427ced7a4d930aa1cad0c918b9d42dd2232d7e
huusz <irinso93@gmail.com>
2017-10-04 21:11:44 +0900

    검색 결과 페이지 완료

--- frontend/src/pages/SearchPage/ResultPage/ResultBox.js
+++ frontend/src/pages/SearchPage/ResultPage/ResultBox.js
@@ -3,6 +3,9 @@ import {
   Grid,
   Image,
   Icon,
+  Label,
+  Card,
+  Header,
 } from 'semantic-ui-react'
 
 const wrapper = {
@@ -13,7 +16,7 @@ const wrapper = {
   verticalAlign: 'middle',
 }
 
-const supText = {
+const message = {
   width: '128px',
   height: '20px',
   fontSize: '14px',
@@ -26,6 +29,23 @@ const icon = {
   width: '13.5px',
   height: '13.5px',
   color: '#54698d',
+  marginRight: '12.3px',
+}
+
+const messagewrap = {
+  height: '28px',
+  padding: '0',
+  position: 'absolute',
+  top: '15px',
+  left: '14px',
+  lineHeight: '28px',
+}
+
+const ImageWrap = {
+  position: 'absolute',
+  top: '54px',
+  left: '14px',
+  padding: '0',
 }
 
 const ImageSize = {
@@ -42,40 +62,304 @@ class ResultBox extends Component {
   render() {
     return (
       <Grid style={wrapper}>
-        <Grid.Row
-          style={{
-            height: '28px',
-          }}>
-          <span style={supText}>
+        <Grid.Row style={messagewrap}>
+          <span style={message}>
             ‘팬케익’ 검색 결과 8건
           </span>
-          <Icon style={icon} name="grid layout" />
-          <Icon style={icon} name="list layout" />
+          <div
+            style={{
+              position: 'absolute',
+              bottom: '7px',
+              right: '25.6px',
+            }}>
+            <Icon
+              style={icon}
+              name="grid layout"
+              size="large"
+            />
+            <Icon
+              style={icon}
+              name="list layout"
+              size="large"
+            />
+          </div>
         </Grid.Row>
-        <Grid.Row columns={4}>
-          <Grid.Column>
-            <Image
-              style={ImageSize}
-              src="https://placeimg.com/279/271/natural"
-            />
+        <Grid.Row columns={4} style={ImageWrap}>
+          <Grid.Column style={{ padding: '0' }}>
+            <div style={{ width: '279px' }}>
+              <Card>
+                <Image
+                  as="a"
+                  style={ImageSize}
+                  src="http://lorempixel.com/279/271/food/"
+                  href="http://google.com"
+                  target="_blank">
+                  {/* <Label
+                content="Image not found!"
+                icon="warning"
+              /> */}
+                </Image>
+                <Card.Content
+                  style={{
+                    width: '279px',
+                    height: '83px',
+                    position: 'absolute',
+                    top: '188px',
+                    fontWeight: '100',
+                    fontSize: '14px',
+                    opacity: '0.7',
+                    borderRadius: '4px',
+                    backgroundImage:
+                      'linear-gradient(253deg, #485563, #29323c)',
+                  }}>
+                  <Card.Header
+                    style={{
+                      color: '#fff',
+                      fontWeight: '100',
+                      position: 'absolute',
+                      top: '13.5px',
+                      left: '18px',
+                      fontSize: '21px',
+                    }}>
+                    베리 팬케익
+                  </Card.Header>
+                  <Card.Meta
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      top: '48px',
+                      left: '18px',
+                    }}>
+                    <Icon name="clock" />
+                    소요시간: 60분
+                  </Card.Meta>
+                  <Card.Description
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      bottom: '15px',
+                      right: '17px',
+                    }}>
+                    <span
+                      style={{
+                        fontFamily:
+                          'Montserrat-Regular',
+                        fontSize: '21px',
+                      }}>
+                      380
+                    </span>{' '}
+                    kcal
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </div>
           </Grid.Column>
-          <Grid.Column>
-            <Image
-              style={ImageSize}
-              src="https://placeimg.com/279/271/natural"
-            />
+          <Grid.Column style={{ padding: '0' }}>
+            <div style={{ width: '279px' }}>
+              <Card>
+                <Image
+                  as="a"
+                  style={ImageSize}
+                  src="http://lorempixel.com/279/271/food/"
+                  href="http://google.com"
+                  target="_blank">
+                  {/* <Label
+                content="Image not found!"
+                icon="warning"
+              /> */}
+                </Image>
+                <Card.Content
+                  style={{
+                    width: '279px',
+                    height: '83px',
+                    position: 'absolute',
+                    top: '188px',
+                    fontWeight: '100',
+                    fontSize: '14px',
+                    opacity: '0.7',
+                    borderRadius: '4px',
+                    backgroundImage:
+                      'linear-gradient(253deg, #485563, #29323c)',
+                  }}>
+                  <Card.Header
+                    style={{
+                      color: '#fff',
+                      fontWeight: '100',
+                      position: 'absolute',
+                      top: '13.5px',
+                      left: '18px',
+                      fontSize: '21px',
+                    }}>
+                    팬케익
+                  </Card.Header>
+                  <Card.Meta
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      top: '48px',
+                      left: '18px',
+                    }}>
+                    <Icon name="clock" />
+                    소요시간: 60분
+                  </Card.Meta>
+                  <Card.Description
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      bottom: '15px',
+                      right: '17px',
+                    }}>
+                    <span
+                      style={{
+                        fontFamily:
+                          'Montserrat-Regular',
+                        fontSize: '21px',
+                      }}>
+                      380
+                    </span>{' '}
+                    kcal
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </div>
           </Grid.Column>
-          <Grid.Column>
-            <Image
-              style={ImageSize}
-              src="https://placeimg.com/279/271/natural"
-            />
+          <Grid.Column style={{ padding: '0' }}>
+            <div style={{ width: '279px' }}>
+              <Card>
+                <Image
+                  as="a"
+                  style={ImageSize}
+                  src="http://lorempixel.com/279/271/food/"
+                  href="http://google.com"
+                  target="_blank">
+                  {/* <Label
+                content="Image not found!"
+                icon="warning"
+              /> */}
+                </Image>
+                <Card.Content
+                  style={{
+                    width: '279px',
+                    height: '83px',
+                    position: 'absolute',
+                    top: '188px',
+                    fontWeight: '100',
+                    fontSize: '14px',
+                    opacity: '0.7',
+                    borderRadius: '4px',
+                    backgroundImage:
+                      'linear-gradient(253deg, #485563, #29323c)',
+                  }}>
+                  <Card.Header
+                    style={{
+                      color: '#fff',
+                      fontWeight: '100',
+                      position: 'absolute',
+                      top: '13.5px',
+                      left: '18px',
+                      fontSize: '21px',
+                    }}>
+                    팬케익
+                  </Card.Header>
+                  <Card.Meta
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      top: '48px',
+                      left: '18px',
+                    }}>
+                    <Icon name="clock" />
+                    소요시간: 60분
+                  </Card.Meta>
+                  <Card.Description
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      bottom: '15px',
+                      right: '17px',
+                    }}>
+                    <span
+                      style={{
+                        fontFamily:
+                          'Montserrat-Regular',
+                        fontSize: '21px',
+                      }}>
+                      380
+                    </span>{' '}
+                    kcal
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </div>
           </Grid.Column>
-          <Grid.Column>
-            <Image
-              style={ImageSize}
-              src="https://placeimg.com/279/271/natural"
-            />
+          <Grid.Column style={{ padding: '0' }}>
+            <div style={{ width: '279px' }}>
+              <Card>
+                <Image
+                  as="a"
+                  style={ImageSize}
+                  src="http://lorempixel.com/279/271/food/"
+                  href="http://google.com"
+                  target="_blank">
+                  {/* <Label
+                content="Image not found!"
+                icon="warning"
+              /> */}
+                </Image>
+                <Card.Content
+                  style={{
+                    width: '279px',
+                    height: '83px',
+                    position: 'absolute',
+                    top: '188px',
+                    fontWeight: '100',
+                    fontSize: '14px',
+                    opacity: '0.7',
+                    borderRadius: '4px',
+                    backgroundImage:
+                      'linear-gradient(253deg, #485563, #29323c)',
+                  }}>
+                  <Card.Header
+                    style={{
+                      color: '#fff',
+                      fontWeight: '100',
+                      position: 'absolute',
+                      top: '13.5px',
+                      left: '18px',
+                      fontSize: '21px',
+                    }}>
+                    팬케익
+                  </Card.Header>
+                  <Card.Meta
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      top: '48px',
+                      left: '18px',
+                    }}>
+                    <Icon name="clock" />
+                    소요시간: 60분
+                  </Card.Meta>
+                  <Card.Description
+                    style={{
+                      color: '#fff',
+                      position: 'absolute',
+                      bottom: '15px',
+                      right: '17px',
+                    }}>
+                    <span
+                      style={{
+                        fontFamily:
+                          'Montserrat-Regular',
+                        fontSize: '21px',
+                      }}>
+                      380
+                    </span>{' '}
+                    kcal
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </div>
           </Grid.Column>
         </Grid.Row>
       </Grid>
