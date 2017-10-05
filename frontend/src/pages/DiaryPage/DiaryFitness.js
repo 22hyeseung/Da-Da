59aa04e1e549afbb239f54fd9bf5fdf16628ddc6
huusz <irinso93@gmail.com>
2017-10-05 21:53:58 +0900

    피트니스탭 퍼블리싱 완료

--- frontend/src/pages/DiaryPage/DiaryFitness.js
+++ frontend/src/pages/DiaryPage/DiaryFitness.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
-import { Segment } from 'semantic-ui-react'
+import {
+  Segment,
+  Header,
+} from 'semantic-ui-react'
+import bgImg from '../../static/img/fitness_bg.png'
+import FitnessIcon from '../../static/img/diary-fitness_default.svg'
+import DiaryFitnessAdd from './DiaryFitnessAdd'
 
 class DiaryFitness extends Component {
   constructor(props) {
@@ -7,7 +13,48 @@ class DiaryFitness extends Component {
   }
 
   render() {
-    return <div>1</div>
+    return (
+      <Segment
+        style={{
+          padding: '33px 36px',
+          boxShadow: 'none',
+          border: '1px solid #D8DDE6',
+          backgroundImage: `url(${bgImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center top',
+          backgroundSize: '34%',
+        }}
+      >
+        <Header
+          style={{
+            fontSize: '28px',
+            fontWeight: '100',
+            color: '#16325C',
+            marginBottom: '42px',
+          }}
+        >
+          <Header.Subheader
+            style={{
+              fontFamily: 'montserrat',
+              fontSize: '14px',
+              fontWeight: '600',
+              color: '#1f2e79',
+            }}
+          >
+            FITNESS DIARY
+          </Header.Subheader>
+          오늘의 운동 다이어리
+          <img
+            src={FitnessIcon}
+            style={{
+              height: '27px',
+              marginBottom: '9px',
+            }}
+          />
+        </Header>
+        <DiaryFitnessAdd />
+      </Segment>
+    )
   }
 }
 
