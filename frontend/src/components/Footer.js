82f9a29c44d78296918f50a5d082af94e5e013e7
feel5ny <feel5.nayoung@gmail.com>
2017-10-07 03:11:15 +0900

    [Global] footer 레이아웃만 완료-footer 디자인 시작

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>© 2017 Da,Da</span>
+      <span>김나영 김세준 이혜승 임옥택 조수현</span>
+    </div>
+  )
+}
+export default Footer
