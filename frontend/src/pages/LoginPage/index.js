5a2261636f194dc5c681393022ffedcb14639996
feel5ny <feel5.nayoung@gmail.com>
2017-10-09 00:12:43 +0900

    Revert "[Login] style 파일 분리 완료"
    This reverts commit 312277c1700d955a49f0f3197fc05100d585aa82.

--- frontend/src/pages/LoginPage/index.js
+++ frontend/src/pages/LoginPage/index.js
@@ -4,10 +4,10 @@ import {
   Header,
   Grid,
 } from 'semantic-ui-react'
+import * as Fa from 'react-icons/lib/fa'
 import './Login.css'
 import bgVideo from '../../static/video/bg_login.mp4'
 import bgImg from '../../static/img/login_img.jpg'
-import * as Style from './StyledLogin'
 
 const LoginPage = () => (
   <div className="login">
@@ -17,27 +17,28 @@ const LoginPage = () => (
     <Grid
       divided="vertically"
       textAlign="left"
-      style={Style.grid}
-    >
+      style={{ marginTop: '127px' }}>
       <Grid.Row columns={4}>
         <Grid.Column>
           <Header
             as="h1"
             className="login-header"
-            style={Style.header}
-          >
+            style={{
+              fontSize: '60px',
+              fontFamily: 'montserrat',
+              fontWeight: '600',
+              color: '#fff',
+            }}>
             <Header.Subheader
-              style={Style.subheader}
-            >
+              style={{
+                color: '#fff',
+                fontSize: '21px',
+              }}>
               건강하고 올바른 <br />
-              <span
-                style={Style.subheaderFirstword}
-              >
+              <span style={{ fontWeight: '700' }}>
                 다
               </span>이어트를 위한 식단 <br />
-              <span
-                style={Style.subheaderFirstword}
-              >
+              <span style={{ fontWeight: '700' }}>
                 다
               </span>이어리 서비스<br />
             </Header.Subheader>
@@ -47,37 +48,43 @@ const LoginPage = () => (
             <Button
               fluid
               style={{
-                ...Style.FbBtn,
-                ...Style.Btn,
-              }}
-            >
+                backgroundImage:
+                  'linear-gradient(261deg, #0f2e6c, #4267b2)',
+                color: '#fff',
+                fontWeight: '100',
+              }}>
+              {/* <Fa.FaFacebookSquare /> */}
               Facebook 계정으로 로그인 하기
             </Button>
             <Button
               fluid
               style={{
-                ...Style.InstaBtn,
-                ...Style.Btn,
-              }}
-            >
+                backgroundImage:
+                  'linear-gradient(279deg, #515bd4, #8134af 23%, #dd2a7b 48%, #feda77)',
+                color: '#fff',
+                fontWeight: '100',
+              }}>
+              {/* <Fa.FaInstagram /> */}
               Instagram 계정으로 로그인 하기
             </Button>
             <Button
               fluid
               style={{
-                ...Style.Btn,
-                ...Style.NaverBtn,
-              }}
-            >
+                backgroundImage:
+                  'linear-gradient(261deg, #008215, #00c73c)',
+                color: '#fff',
+                fontWeight: '100',
+              }}>
               Naver 계정으로 로그인 하기
             </Button>
             <Button
               fluid
               style={{
-                ...Style.Btn,
-                ...Style.KakaoBtn,
-              }}
-            >
+                backgroundImage:
+                  'linear-gradient(261deg, #ffb600, #fae200)',
+                color: '#16325c',
+                fontWeight: '100',
+              }}>
               Kakao 계정으로 로그인 하기
             </Button>
           </div>
