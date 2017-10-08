406b1ff09657f27bd3c62afb9cc235103a1e63bf
downmix <downmix@downmix.net>
2017-10-08 16:37:25 +0900

    Oauth 기본base 구현. >> DB연동, CORS 프론트 연동 후속개발 필요

--- backend/src/router/auth.js
+++ backend/src/router/auth.js
@@ -5,7 +5,7 @@ const cookieSession = require('cookie-session')
 const csurf = require('csurf')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
-//const GitHubStrategy = require('passport-github').Strategy
+const KakaoStrategy = require('passport-kakao').Strategy
 
 const query = require('../query')
 const mw = require('../middleware')
@@ -26,11 +26,14 @@ router.use(passport.initialize())
 router.use(passport.session())
 
 passport.serializeUser((user, done) => {
-  done(null, `${user.provider}:${user.provider_user_id}`)
+  done(null, `${user.member_provider}:${user.member_provider_number}`)
 })
 
 passport.deserializeUser((str, done) => {
-  const [provider, provider_user_id] = str.split(':')
+  // 임시 code
+  console.log(str, '<< [ str ]');
+  done(null, str);
+  /*const [provider, provider_user_id] = str.split(':')
   query.firstOrCreateUserByProvider(provider, provider_user_id)
     .then(user => {
       if (user) {
@@ -39,45 +42,61 @@ passport.deserializeUser((str, done) => {
         done(new Error('해당 정보와 일치하는 사용자가 없습니다.'))
       }
     })
+  */
 })
-/*
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL
+
+passport.use(new KakaoStrategy({
+  clientID: process.env.KAKAO_CLIENT_ID,
+  clientSecret: process.env.KAKAO_CLIENT_SECRET,
+  callbackURL: process.env.KAKAO_CALLBACK_URL
 }, (accessToken, refreshToken, profile, done) => {
+  const avatar_url = profile._json.properties.profile_image ? profile._json.properties.profile_image : null;
+  const user_name = profile.displayName ? profile.displayName : null;
+
+  console.log(avatar_url, user_name, '<< [ avatar_url, user_name ]');
+  // 임시 data, code
+  const user = {
+    "member_provider": "kakao",
+    "member_provider_number": profile.id,
+    "member_provider_name": user_name,
+    "member_avatar_url" : avatar_url
+  };
+  done(null, user);
+/*
   query.firstOrCreateUserByProvider(
-    'github',
+    'kakao',
     profile.id,
-    accessToken
+    accessToken,
+    avatar_url,
+    user_name
   ).then(user => {
     done(null, user)
   }).catch(err => {
     done(err)
   })
-}))
 */
-router.get('/', (req, res) => {
-  res.render('auth.pug')
-})
+}))
 
-// 로그인 성공 시
+router.get('/', (req, res) => {
+  res.render('auth.pug');
+});
+
+// 로그인 성공
 router.get('/success', mw.loginRequired, (req, res) => {
-  const token = jwt.sign({id: req.user.id}, process.env.JWT_SECRET)
+  const token = jwt.sign({id: req.user.id}, process.env.JWT_SECRET);
+
   res.render('success.pug', {
     token,
     origin: process.env.TARGET_ORIGIN
   })
 })
 
-router.get('/github', passport.authenticate('github'))
+router.get('/kakao', passport.authenticate('kakao'))
 
-router.get('/github/callback', (req, res, next) => {
-  // successRedirect, failureRedirect 지정하는 대신 직접 처리하기
-  // http://passportjs.org/docs#custom-callback
-  passport.authenticate('github', (err, user, info) => {
-    // 예상치 못한 예외 발생 시
+router.get('/kakao/callback', (req, res, next) => {
+  passport.authenticate('kakao', (err, user, info) => {
     if (err) {
+      // 예상치 못한 예외 발생 시
       return next(err)
     }
     if (!user) {
@@ -86,16 +105,17 @@ router.get('/github/callback', (req, res, next) => {
     }
     req.logIn(user, err => {
       // 예상치 못한 예외 발생 시
-      if (err) {
+      if(err){
         return next(err)
       }
-      // 로그인 성공 시
+      // 로그인 성공
       res.redirect(req.baseUrl + '/success')
     })
   })(req, res, next)
 })
 
 router.use((err, req, res, next) => {
+  console.log(err, err.message, '<< [ err, err.message ]');
   res.redirect(req.baseUrl)
 })
 
