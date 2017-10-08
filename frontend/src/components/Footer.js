efda642a8e5cfa0247cb6a78f39ca26eaac8c822
feel5ny <feel5.nayoung@gmail.com>
2017-10-09 01:51:36 +0900

    [Global] footer 완료

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
+const routes = [
+  {
+    linkLabel: '다이어리',
+    linkTo: '/diary',
+  },
+  {
+    linkLabel: '리포트',
+    linkTo: '/report',
+  },
+  {
+    linkLabel: '체중 기록',
+    linkTo: '/weight',
+  },
+  {
+    linkLabel: '레시피 검색',
+    linkTo: '/search',
+  },
+]
+
+const link = {
+  color: '#A8B7C7',
+  marginLeft: '10px',
+}
 const Footer = () => {
   return (
     <div className="footer">
-      <span>© 2017 Da,Da</span>
-      <span>김나영 김세준 이혜승 임옥택 조수현</span>
+      <span>
+        © 2017
+        <a
+          href="https://github.com/huusz/Da-Da"
+          style={link}
+        >
+          Da,Da
+        </a>
+      </span>
+      <div>
+        {routes.map(route => (
+          <span>
+            <Link style={link} to={route.linkTo}>
+              {route.linkLabel}
+            </Link>
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
