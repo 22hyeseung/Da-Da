c20eabdc2c628c10576819dae038a2dc2818cf6a
kimsejune <tpwns1088@gmail.com>
2017-10-10 19:53:21 +0900

    knex migrate recipe, food table
    food, recipe table 생성

--- backend/migrations/20171007175704_recipe.js
+++ backend/migrations/20171007175704_recipe.js
@@ -6,8 +6,9 @@ exports.up = function(knex, Promise) {
     t.string('recipe_name_en').notNullable() // 레시피 제목(영어)
     t.string('recipe_level').notNullable() // 난이도
     t.string('recipe_time').notNullable() // 소요시간
-    t.integer('recipe_kcal').notNullable() // 칼로리
-    t.string('recipe_nutrient').notNullable() // 영양소 [{"탄수화물": "80g", "단백질": "40g", "지방":"20g"}]
+    t.float('recipe_carb').notNullable() // 탄수화물
+    t.float('recipe_protein').notNullable() // 단백질
+    t.float('recipe_fat').notNullable() // 지방
     t.string('recipe_ingredient').notNullable() // 재료 [{"감자": "10g", "물": "10", "오이":"10g"}]
     t.string('recipe').notNullable() // 요리법 [{"1": "ㅁㄴㅇㅁㅇㅁㄴ", "2": "ㅁㄴㅇㅁㄴㄴㅁ", "3":"ㅁㄴㅇㅁㄴㅇㅁㄴㅇ"}]
     t.integer('recipe_serving').notNullable() // 요리법 인분
