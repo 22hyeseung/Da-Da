7c7b9cc08016b6d6b0e2fbc4f369d26c7c4fb16d
kimsejune <tpwns1088@gmail.com>
2017-10-10 21:09:57 +0900

    knex migrate member table update
    member table SNS이름, SNS 사진경로,  나이 , 회원 성별 null 허

--- backend/migrations/20171010195406_member.js
+++ backend/migrations/20171010195406_member.js
@@ -4,11 +4,11 @@ exports.up = function(knex, Promise) {
     t.increments('member_id') // 일련번호(PK)
     t.string('member_provider').notNullable()  // SNS그룹
     t.string('member_provider_number').notNullable()  // SNS ID
-    t.string('member_provider_name').notNullable()  // SNS 이름
-    t.string('member_avatar_url').notNullable()  // SNS 사진경로
-    t.integer('member_age').notNullable()  // 나이
+    t.string('member_provider_name')  // SNS 이름
+    t.string('member_avatar_url')  // SNS 사진경로
+    t.integer('member_age')  // 나이
     t.timestamp('member_join_date').defaultTo(knex.fn.now())  // 가입일
-    t.enum('member_gender', ['남', '여']).notNullable()  // 회원 성별
+    t.enum('member_gender', ['남', '여'])  // 회원 성별
     t.string('token')  // SNS JWT 토큰(보류)
   })
 };
