9abefd312830e2838089c6257cb5079668e10dd0
kimsejune <tpwns1088@gmail.com>
2017-10-10 20:23:33 +0900

    knex migrate exercise table
    exercise table 생성

---
+++ backend/migrations/20171010202040_exercise.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('exercise', t => {
+    t.increments('exercise_id')  // 일련번호(PK)
+    t.float('exercise_burn_kcal').notNullable() // 소모칼로리(1분)
+    t.string('exercise_name').notNullable() // 운동 이름
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('exercise')
+};
