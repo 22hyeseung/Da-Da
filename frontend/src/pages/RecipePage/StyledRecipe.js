9168cb2294640684d4e405dbdcf6762e99918cb9
huusz <irinso93@gmail.com>
2017-10-11 13:51:52 +0900

    레시피 페이지 네비게이션 레이아웃 조정

--- frontend/src/pages/RecipePage/StyledRecipe.js
+++ frontend/src/pages/RecipePage/StyledRecipe.js
@@ -19,6 +19,12 @@ export const topContainer = {
   position: 'relative',
 }
 
+export const navigationGrid = {
+  width: '1200px',
+  margin: '0 auto',
+  position: 'relative',
+}
+
 export const bottomContainer = {
   height: '555px',
   position: 'relative',
