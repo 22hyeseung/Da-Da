fc0ae8edda01ff2125575b00b6d177b83d2c37aa
kimsejune <tpwns1088@gmail.com>
2017-10-13 12:46:55 +0900

    day_log의 colum 제약사항 변경
    height, kg, age, kcal null 허용

--- backend/migrations/20171010201320_day_log.js
+++ backend/migrations/20171010201320_day_log.js
@@ -4,10 +4,10 @@ exports.up = function(knex, Promise) {
     t.increments('day_log_id')  //  변화 id(PK)
     t.integer('day_log_member_id').unsigned()  // 멤버 id(FK)
     t.foreign('day_log_member_id').references('member.member_id')
-    t.float('day_log_height').notNullable()  // 키
-    t.float('day_log_kg').notNullable()  // 몸무게
-    t.integer('day_log_age').notNullable()  // 나이
-    t.integer('day_log_kcal').notNullable()  // 목표 칼로리
+    t.float('day_log_height')  // 키
+    t.float('day_log_kg')  // 몸무게
+    t.integer('day_log_age')  // 나이
+    t.integer('day_log_kcal')  // 목표 칼로리
     t.string('day_log_regret')  // 반성일기
     t.string('day_log_comment')  // 일기 내용
     t.date('day_log_diary_date') // 등록일
