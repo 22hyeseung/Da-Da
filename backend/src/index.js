4a57bb1d30cefcbe38f4e0bdd333ec8bc8758327
Suhyeon JO <flora12375@gmail.com>
2017-10-13 12:40:07 +0900

    index exercise router 적용

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const express = require('express')
 const authRouter = require('./router/auth')
 const userRouter = require('./router/user')
+const exerciseRouter = require('./router/exercise')
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.set('trust proxy')
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/auth', authRouter)
 app.use('/user', userRouter)
+app.use('/exercise', exerciseRouter)
 
 app.listen(PORT, () => {
   console.log(`listening ${PORT}...`)
