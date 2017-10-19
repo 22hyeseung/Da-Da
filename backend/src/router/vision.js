2420d7bc928b6ac5fd99a8e54fbf7a79751dc23a
downmix <downmix@downmix.net>
2017-10-20 01:20:32 +0900

    비동기 처

--- backend/src/router/vision.js
+++ backend/src/router/vision.js
@@ -77,6 +77,31 @@ function googleVision(fileUrl) {
   })
 }
 
+function s3upload(file, ext) {
+  return new Promise((resolve, reject) => {
+    s3.upload({
+      'ACL': 'public-read', // 익명의 사용자도 파일 경로만 알면 읽기 가능하도록 설정
+      'Body': file.buffer,
+      'Bucket': process.env.S3_BUCKET_NAME,
+      'Key': `${uuid.v4()}.${ext}`, // 파일이름
+      'ContentDisposition': 'inline', // Content-Disposition 헤더
+      'ContentType': 'string' // Content-Type 헤더 ??????
+    }, (err, s3_file) => {
+      console.log(s3_file, '<< [ s3_file ]');
+      if (err) {
+        reject(err)
+        // res.status(500)
+        // res.send(err)
+      } else {
+        resolve(s3_file)
+        // S3 업로드 완료되자마자 곧바로 google vision으로 보내게 되면, 간혹 s3파일을 못 불러올때가 있음.
+        // s3 파일을 갱신타임이 존재하는것으로 보임.
+        // 서버 딜레이를 주기위한 방법(settimeout, delay 관련 npm) 적용필요
+      }
+    })
+  })
+}
+
 router.get('/debug', (req, res) => {
   res.render('vision-debug.pug')
 })
@@ -84,7 +109,6 @@ router.get('/debug', (req, res) => {
 router.post('/', upload.single('upload_img'), (req, res) => {
   // jpg image/jpeg << [ ext, mime ]
   const { ext, mime } = fileType(req.file.buffer)
-
   if (!supportImageExt.includes(ext)) {
     res.status(400)
     res.send('지원하지 않는 파일입니다.')
@@ -93,36 +117,14 @@ router.post('/', upload.single('upload_img'), (req, res) => {
     res.send('파일 용량은 3mb 까지 입니다.')
   }
 
-  s3.upload({
-    'ACL': 'public-read', // 익명의 사용자도 파일 경로만 알면 읽기 가능하도록 설정
-    'Body': req.file.buffer,
-    'Bucket': process.env.S3_BUCKET_NAME,
-    'Key': `${uuid.v4()}.${ext}`, // 파일이름
-    'ContentDisposition': 'inline', // Content-Disposition 헤더
-    'ContentType': mime // Content-Type 헤더
-  }, (err, s3_file) => {
-    if (err) {
-      res.status(500)
-      res.send(err)
-    } else {
-      // S3 업로드 완료되자마자 곧바로 google vision으로 보내게 되면, 간혹 s3파일을 못 불러올때가 있음.
-      // s3 파일을 갱신타임이 존재하는것으로 보임.
-      // 서버 딜레이를 주기위한 방법(settimeout, delay 관련 npm) 적용필요
-
-      googleVision(s3_file.Location)
-        .then(result => {
-          // 최종성공
-          res.render('vision.pug', {
-            'visionAnalysis': JSON.stringify(result),
-            'imgUrl': s3_file.Location
-          })
-        })
-        .catch(err => {
-          res.status(500)
-          res.send(err)
-        })
-    }
-  })
+  Promise.all([googleVision(req.file.buffer), s3upload(req.file, ext)])
+    .then(result => {
+      console.log('모두 완료', result)
+      res.render('vision.pug', {
+        'visionAnalysis': JSON.stringify(result[0]),
+        'imgUrl': result[1].Location
+      })
+    })
 })
 
 module.exports = router
