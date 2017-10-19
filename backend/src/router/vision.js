bb36ef9bc016e6f33b1e80b1705cc581c5c85068
downmix <downmix@downmix.net>
2017-10-19 17:08:54 +0900

    구글 vision + AWS s3 연동

--- backend/src/router/vision.js
+++ backend/src/router/vision.js
@@ -7,20 +7,32 @@ const fs = require('fs')
 const util = require('util')
 const mime = require('mime')
 const multer = require('multer')
+const aws = require('aws-sdk')
+const uuid = require('uuid')
+const fileType = require('file-type')
 
-const upload = multer({ 'dest': 'upload_file/' })
-
+/**
+ * Google Vision
+ */
 const gcloud = require('google-cloud')({
   'keyFilename': 'gkey.json',
   'projectId': 'my-project-1470459490848'
 })
-
 const vision = gcloud.vision()
 
+/**
+ * AWS S3
+ */
+const upload = multer({ 'storage': multer.memoryStorage() })
+const s3 = new aws.S3({ 'apiVersion': '2006-03-01' })
+
+const supportImageExt = ['png', 'jpg']
 const router = express.Router()
 
-// 도메인 CORS 제약조건 해제
-// 실서버 적용시 적용 검토필요
+/**
+ * 도메인 CORS 제약조건 해제
+ * 실서버 적용시 적용 검토필요
+ */
 // router.use(cors({ 'origin': process.env.TARGET_ORIGIN }))
 
 router.use((req, res, next) => {
@@ -29,8 +41,10 @@ router.use((req, res, next) => {
 
 router.use(bodyParser.json())
 
-// 토큰유효성 검사 해제
-// 실서버 적용시 적용 검토필요
+/**
+ * 토큰유효성 검사 해제
+ * 실서버 적용시 적용 검토필요
+ */
 // router.use(expressJwt({ 'secret': process.env.JWT_SECRET }))
 
 function base64Image(src) {
@@ -38,37 +52,70 @@ function base64Image(src) {
   return util.format('data:%s;base64,%s', mime.getType(src), data)
 }
 
+function googleVision(fileUrl) {
+  return new Promise((resolve, reject) => {
+    // faces, landmarks, labels, logos, properties, safeSearch, texts
+    const types = ['labels']
+    console.log(fileUrl, '<< [ fileUrl ]');
+
+    vision.detect(fileUrl, types, (err, detections, apiResponse) => {
+      console.log(err, '<< [ err ]');
+      if (err) {
+        reject(err)
+      } else {
+        const apiData = []
+
+        apiResponse.responses[0].labelAnnotations.map(val => {
+          apiData.push({
+            'description': val.description,
+            'score': val.score
+          })
+        })
+        console.log(apiData, '<< [ apiData ]')
+
+        resolve(apiData)
+      }
+    })
+  })
+}
+
 router.get('/debug', (req, res) => {
-  res.render('vision.pug')
+  res.render('vision-debug.pug')
 })
 
 router.post('/', upload.single('upload_img'), (req, res) => {
-  // faces, landmarks, labels, logos, properties, safeSearch, texts
-  const types = ['labels']
+  // jpg image/jpeg << [ ext, mime ]
+  const file = req.file
+  const { ext, mime } = fileType(file.buffer)
 
-  vision.detect(req.file.path, types, (err, detections, apiResponse) => {
+  if (!supportImageExt.includes(ext)) {
+    console.log('<< [ xxx ]')
+  } else if (file.size > (1024 * 1024 * 5)) {
+    console.log('<< [ 용량 초과 ]')
+  }
+
+  s3.upload({
+    'ACL': 'public-read', // 익명의 사용자도 파일 경로만 알면 읽기 가능하도록 설정
+    'Body': file.buffer,
+    'Bucket': process.env.S3_BUCKET_NAME,
+    'Key': `${uuid.v4()}.${ext}`, //파일이름
+    'ContentDisposition': 'inline', // Content-Disposition 헤더
+    'ContentType': mime // Content-Type 헤더
+  }, (err, s3_file) => {
     if (err) {
-      res.end('Errorrrr')
+      console.log(err, '<< [ err ]')
     } else {
-      const apiData = []
+      console.log(s3_file, '<< [ s3_file ]')
 
-      apiResponse.responses[0].labelAnnotations.map(val => {
-        apiData.push({
-          'description': val.description,
-          'score': val.score
+      googleVision(s3_file.Location).then(result => {
+        res.render('vision.pug', {
+          'visionAnalysis': JSON.stringify(result),
+          'imgUrl': s3_file.Location
         })
       })
-
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.write('<html><meta charset="UTF-8"><body>')
-      res.write(`<img max-width: 100%; src="${base64Image(req.file.path)}"><br>`)
-      res.write(JSON.stringify(apiData))
-
-      fs.unlinkSync(req.file.path)
-
-      res.end('</body></html>')
     }
   })
+
 })
 
 module.exports = router
