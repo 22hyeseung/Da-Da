0148ede1daeaac45721f7d73204052340b3c295a
kimsejune <tpwns1088@gmail.com>
2017-10-22 10:47:08 +0900

    eat_log_serve추가

--- backend/src/query.js
+++ backend/src/query.js
@@ -215,7 +215,7 @@ function getEatLogsFood({ eat_log_member_id, eat_log_diary_date }) {
 function getEatLogsRecipe({ eat_log_member_id, eat_log_diary_date }) {
   return knex('eat_log')
     .select(
-      'eat_log_id', 'eat_log.eat_log_picture', 'eat_log.eat_log_recipe_id', 'recipe.recipe_name_ko', 'recipe.recipe_name_en', 'eat_log.eat_log_meal_tag',
+      'eat_log_id', 'eat_log.eat_log_picture', 'eat_log.eat_log_recipe_id', 'eat_log.eat_log_serve', 'recipe.recipe_name_ko', 'recipe.recipe_name_en', 'eat_log.eat_log_meal_tag',
       knex.raw('((((recipe.recipe_carb * 4) + (recipe.recipe_protein * 4) + (recipe.recipe_fat * 9))/recipe.recipe_serving)*eat_log.eat_log_serve) as recipe_kcal'),
       knex.raw('((recipe.recipe_carb/recipe.recipe_serving) * eat_log.eat_log_serve ) as recipe_carb'),
       knex.raw('((recipe.recipe_protein/recipe.recipe_serving) * eat_log.eat_log_serve) as recipe_protein'),
