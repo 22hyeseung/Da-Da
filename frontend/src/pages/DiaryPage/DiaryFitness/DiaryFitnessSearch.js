5309885fb6689942f4b3d9a1f9967163d7af0860
feel5ny <feel5.nayoung@gmail.com>
2017-10-24 17:09:58 +0900

    검색 결과 렌더링 UI 퍼블, 스타일 완료

--- frontend/src/pages/DiaryPage/DiaryFitness/DiaryFitnessSearch.js
+++ frontend/src/pages/DiaryPage/DiaryFitness/DiaryFitnessSearch.js
@@ -7,8 +7,11 @@ import {
 } from 'semantic-ui-react'
 import * as Style from './StyledFitness'
 import multiplyIcon from '../../../static/img/diary-multiply.svg'
+import '../Diary.css'
 import returnIcon from '../../../static/img/diary-return.svg'
 import rootApi from '../../../config'
+import notyet from '../../../static/img/diary-fitness-search-notyet.svg'
+import error from '../../../static/img/diary-search-error.svg'
 import { connect } from 'react-redux'
 
 class DiaryFitnessSearch extends React.Component {
@@ -88,38 +91,30 @@ class DiaryFitnessSearch extends React.Component {
           <List.Item
             style={Style.listItemLeftWrap}
           >
-            <List.Content>
-              <Input
-                icon="search"
-                placeholder="어떤 종류의 운동을 하셨나요?"
-                style={Style.inputStyle}
-                onChange={this.handleChange}
-                value={this.state.userInput}
-                onKeyDown={this.handleKeyPress}
-              />
-              <img
-                src={multiplyIcon}
-                alt="곱하기 모양의 아이콘입니다."
-              />
-            </List.Content>
-            <ul>
-              {this.state.results.map(
-                (result, i) => {
-                  return (
-                    <li>
-                      {result.exercise_name}
-                      {result.exercise_burn_kcal}
-                    </li>
-                  )
-                },
-              )}
-            </ul>
-            <List.Content />
+            <Input
+              icon="search"
+              placeholder="어떤 종류의 운동을 하셨나요?"
+              className="diary-fitness-search"
+              style={Style.inputStyle}
+              onChange={this.handleChange}
+              value={this.state.userInput}
+              onKeyDown={this.handleKeyPress}
+            />
+            <img
+              src={multiplyIcon}
+              alt="곱하기 모양의 아이콘입니다."
+              style={{ margin: '0px 18px' }}
+            />
           </List.Item>
           <List.Item
             style={Style.listItemRightWrap}
           >
-            <List.Content>
+            <List.Content
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
               <Input placeholder="얼마나 운동하셨나요?" />
               <img
                 src={returnIcon}
@@ -134,19 +129,179 @@ class DiaryFitnessSearch extends React.Component {
               style={Style.textSmall_Input}
             >
               <span style={Style.textBig}>
-                0{' '}
+                0
               </span>kcal
             </List.Content>
           </List.Item>
-          <div style={Style.buttonWrap}>
-            <Button basic style={Style.button}>
-              취소
-            </Button>
-            <Button style={Style.submitButton}>
-              등록
-            </Button>
-          </div>
         </List>
+        <List
+          style={{
+            width: '54%',
+            marginLeft: '14px',
+          }}
+        >
+          <ul
+            style={{
+              listStyle: 'none',
+
+              paddingRight: '14px',
+            }}
+          >
+            {!this.state.isEmpty ? (
+              <div>
+                {this.state.results.length !==
+                0 ? (
+                  <div>
+                    <div
+                      style={
+                        Style.searchResultWrapper
+                      }
+                    >
+                      <span
+                        style={Style.searchResult}
+                      >
+                        검색결과{' '}
+                        {
+                          this.state.results
+                            .length
+                        }
+                      </span>
+                      <span />
+                    </div>
+                    <div
+                      style={{
+                        height: '185px',
+                        overflow: 'auto',
+                      }}
+                    >
+                      {this.state.results.map(
+                        (result, i) => {
+                          return (
+                            <li
+                              style={{
+                                display: 'flex',
+                                justifyContent:
+                                  'space-between',
+                                padding: '7px',
+                                margin: '7px 0px',
+                                borderBottom:
+                                  '1px solid #d8dde6',
+                                cursor: 'pointer',
+                              }}
+                            >
+                              <span
+                                style={{
+                                  fontColor:
+                                    '#16325c',
+                                }}
+                              >
+                                {
+                                  result.exercise_name
+                                }
+                              </span>
+                              <span
+                                style={{
+                                  fontFamily:
+                                    'Montserrat-bold',
+                                  fontSize:
+                                    '28px',
+                                }}
+                              >
+                                {result.exercise_burn_kcal}{' '}
+                                <span
+                                  style={{
+                                    fontFamily:
+                                      'Montserrat-bold',
+                                    fontSize:
+                                      '14px',
+                                  }}
+                                >
+                                  kcal
+                                </span>
+                                <span
+                                  style={{
+                                    fontSize:
+                                      '14px',
+                                  }}
+                                >
+                                  /분
+                                </span>
+                              </span>
+                            </li>
+                          )
+                        },
+                      )}
+                    </div>
+                  </div>
+                ) : (
+                  <div style={Style.noResult}>
+                    <div
+                      style={
+                        Style.noResultWrapper
+                      }
+                    >
+                      <span
+                        style={{
+                          ...Style.noResultMsg,
+                          marginTop: '10px',
+                        }}
+                      >
+                        검색결과가 없습니다.
+                      </span>
+                      <span
+                        style={Style.noResultMsg}
+                      >
+                        좀 더 큰 범위의 키워드로 검색해보세요
+                      </span>
+                    </div>
+                    <img
+                      style={Style.errorIcon}
+                      src={error}
+                      alt="검색결과가 없어서 표시하는 이미지입니다"
+                    />
+                  </div>
+                )}
+              </div>
+            ) : (
+              // 3. 초기 빈 화면
+              <div style={Style.searchDefault}>
+                <div
+                  style={{ textAlign: 'center' }}
+                >
+                  <span
+                    style={{
+                      ...Style.searchDefaultMsg,
+                      display: 'block',
+                    }}
+                  >
+                    어떤 운동을 하셨나요?
+                  </span>
+                  <span
+                    style={Style.searchDefaultMsg}
+                  >
+                    운동 종류를 검색해보세요!
+                  </span>
+                </div>
+                <img
+                  style={{
+                    width: '26%',
+                    marginTop: '21px',
+                  }}
+                  src={notyet}
+                  alt="검색 전 검색을 유도하는 이미지입니다"
+                />
+              </div>
+            )}
+          </ul>
+        </List>
+        <div style={Style.buttonWrap}>
+          <Button basic style={Style.button}>
+            취소
+          </Button>
+          <Button style={Style.submitButton}>
+            등록
+          </Button>
+        </div>
         <Divider />
       </div>
     )
--- frontend/src/pages/DiaryPage/DiaryFitness/StyledFitness.js
+++ frontend/src/pages/DiaryPage/DiaryFitness/StyledFitness.js
@@ -2,7 +2,7 @@ import bgImg from '../../../static/img/fitness_bg.png'
 
 // ==========================> index.js
 export const container = {
-  height: '574px',
+  // height: '574px',
   overflow: 'auto',
   padding: '33px 36px',
   boxShadow: 'none',
@@ -21,7 +21,7 @@ export const icon = {
 
 // ==========================> DiaryFitnessList.js
 
-export const listWrap = { width: '95%' }
+export const listWrap = { width: '100%' }
 
 export const listItemLeft = {
   width: '20%',
@@ -49,6 +49,7 @@ export const textBig = {
   letterSpacing: '-1px',
   color: '#16325c',
   fontFamily: 'Montserrat-Bold',
+  marginRight: '7px',
 }
 
 export const buttonIcon = {
@@ -97,8 +98,8 @@ export const textSmall_Input = {
 }
 
 export const buttonWrap = {
-  float: 'right',
-  marginTop: '40px',
+  display: 'flex',
+  justifyContent: 'flex-end',
 }
 
 export const button = {
@@ -130,3 +131,66 @@ export const addButton = {
 }
 
 export const addIcon = { marginRight: '10px' }
+
+// ==========================> DiaryFitnessSearch.js
+export const errorIcon = {
+  width: '6%',
+  filter: 'grayscale(100%)',
+}
+
+export const noResultMsg = {
+  color: '#a8b7c7',
+  fontWeight: '100',
+  display: 'block',
+}
+
+export const noResult = {
+  display: 'flex',
+  height: '169px',
+  flexDirection: 'column-reverse',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+export const noResultWrapper = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+}
+
+export const resultSmallMsg = {
+  width: '100%',
+  fontSize: '12px',
+  padding: '7px 0px',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  color: '#a8b7c7',
+}
+
+export const searchResult = {
+  width: '100%',
+  display: 'inline-block',
+  padding: '7px 0px',
+  color: '#54698d',
+  marginBottom: '7px',
+}
+
+export const searchResultWrapper = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  borderBottom: '1px dashed #a8b7c7',
+}
+
+export const searchDefaultMsg = {
+  color: '#a8b7c7',
+  fontWeight: '100',
+  marginTop: '10px',
+}
+
+export const searchDefault = {
+  display: 'flex',
+  height: '100%',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'center',
+}
