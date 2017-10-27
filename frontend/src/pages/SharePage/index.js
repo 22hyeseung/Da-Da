5b31292cb652a558d295e828d4f769c84466149e
downmix <downmix@downmix.net>
2017-10-27 16:33:13 +0900

    URL 파라미터 기반구축. 중간커밋

--- frontend/src/pages/SharePage/index.js
+++ frontend/src/pages/SharePage/index.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Share.css'
 import './ShareDiary.css'
 import ShareDiaryFood from './ShareDiaryFood'
 import ShareDiaryFitness from './ShareDiaryFitness'
 import ShareTitle from './ShareTitle'
 
-const SharePage = () => {
-  return (
-    <div className="share-wrapper">
-      <div className="share-grid">
-        <span className="share-bg-logo">
-          DA, DA
-        </span>
-        <ShareTitle />
-        <ShareDiaryFood />
-        <ShareDiaryFitness />
+class SharePage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      params: new URLSearchParams(props.location.search),
+    }
+  }
+
+  render() {
+    console.log(this.state.params.get('user_id'), '<< [ this.state.params.get(user_id) ]');
+    return (
+      <div className="share-wrapper">
+        <div className="share-grid">
+          <span className="share-bg-logo">
+            DA, DA
+          </span>
+          <ShareTitle />
+          <ShareDiaryFood />
+          <ShareDiaryFitness />
+        </div>
+        <footer className="share-footer">
+          나만의 식단다이어리를 갖고 싶다면
+          <span className="share-footer-logo">
+            DA,DA
+          </span>
+          를 찾으세요!
+        </footer>
       </div>
-      <footer className="share-footer">
-        나만의 식단다이어리를 갖고 싶다면
-        <span className="share-footer-logo">
-          DA,DA
-        </span>
-        를 찾으세요!
-      </footer>
-    </div>
-  )
+    )
+  }
 }
 
 export default SharePage
