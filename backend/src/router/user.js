e0a7aa1d9e35e1ded7316731a8d1b8f6b91978b7
kimsejune <tpwns1088@gmail.com>
2017-11-01 13:39:25 +0900

    json, urlencoded, expressjwt, cors Middleware user 적용

--- backend/src/router/user.js
+++ backend/src/router/user.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const expressJwt = require('express-jwt')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
 const query = require('../query')
+const mw = require('../middleware')
 
 const router = express.Router()
 
@@ -11,15 +8,15 @@ const router = express.Router()
  * @apiDefine user user
  */
 
-router.use(cors({ 'origin': process.env.TARGET_ORIGIN }))
+router.use(mw.corsMiddleware)
 
 router.use((req, res, next) => {
   next()
 })
 
-router.use(bodyParser.json())
-router.use(expressJwt({ 'secret': process.env.JWT_SECRET }))
-router.options('*', cors())
+router.use(mw.jsonMiddleware)
+router.use(mw.expressJwtMiddleware)
+router.options('*', mw.corsMiddleware)
 
 /**
  * @api {get} /user Get UserData
